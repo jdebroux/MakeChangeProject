20dbe5f0f66244c8087f97d4f0b5c4a1d8f8d547
jdebroux <jtdebroux@gmail.com>
2019-07-12 16:53:10 -0600

    Initial commit

---
+++ src/com/skilldistillery/makechange/MakeChangeApp.java
@@ -0,0 +1,12 @@
+package com.skilldistillery.makechange;
+
+import java.util.Scanner;
+
+public class MakeChangeApp {
+
+	public static void main(String[] args) {
+		Scanner kb = new Scanner(System.in);
+		System.out.print("How much is the item being purchased? ");
+	}
+
+}
