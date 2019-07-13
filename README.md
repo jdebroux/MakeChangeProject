078c952421021c0c9889294e63aadd39c373b55b
jdebroux <jtdebroux@gmail.com>
2019-07-12 18:34:51 -0600

    updated README and working on change equations

--- README.md
+++ README.md
@@ -3,7 +3,7 @@
 Week 1 homework for Skill Distillery Java bootcamp.
 
 ### Overview
-
+This project uses basic operations to take the cost of an item and the tender amount the user is paying with to determine the amount of change the user should get for that purchase.  Try it out :)!
 ### Technologies Used
 
 * Java
@@ -12,3 +12,5 @@ Week 1 homework for Skill Distillery Java bootcamp.
 * Git
 
 ### Lessons Learned
+
+* multiple methods can make things more organized.
--- src/com/skilldistillery/makechange/MakeChangeApp.java
+++ src/com/skilldistillery/makechange/MakeChangeApp.java
@@ -11,25 +11,66 @@ public class MakeChangeApp {
 		boolean hasEnough$ = tooLittleOrExactMoney(cost, payment);
 		if (hasEnough$ == false) {
 			System.exit(0);
-		}//end if statement
+		} // end if statement
 		else {
-			
-		}// end of else
+			System.out.print("Your change will be: ");
+			int change = changeEquation(cost, payment);
+			whatChangeWillBe(change);
+
+//			System.out.println(cost);
+//			System.out.println(payment);
+//			System.out.println(change);
+
+		} // end of else
 		kb.close();
 	}// end of main
+
+	private static void whatChangeWillBe(int change) {
+		if (change >= 1000) {
+			change -= 1000;
+			System.out.print("1 ten, ");
+		}
+		if (change >= 500) {
+			change -= 500;
+			System.out.print("1 five, ");
+		}
+		if (change >= 400) {
+			change -= 400;
+			System.out.println("4 dollars, ");
+		}else if(change >= 300) {
+			change -= 300;
+			System.out.println("3 dollars, ");
+		}else if(change >= 200) {
+			change -= 200;
+			System.out.println("2 dollars, ");
+		}else if(change >= 100) {
+			change -= 100;
+			System.out.println("1 dollar, ");
+		}
+		System.out.println(change);
+//		if (change )
+		
+	}// end of whatChangeWillBe
+
+	private static int changeEquation(double cost, double payment) {
+		cost *= 100;
+		payment *= 100;
+		int change = (int) (payment - cost);
+		return change;
+	}// end changeEquation
 
 	private static boolean tooLittleOrExactMoney(double cost, double payment) {
 		if (payment < cost) {
 			System.out.println("You cannot buy you this item, you are too poor.");
 			return (false);
-		}//end if statement
+		} // end if statement
 		else if (payment == cost) {
 			System.out.println("You paid with exact change.  Have a terrific day!");
 			return (false);
-		}//end else if
+		} // end else if
 		else {
 			return (true);
-		}//end else
+		} // end else
 	}// end tooLittleMoney
 
 	private static double askUser1(Scanner user1) {
