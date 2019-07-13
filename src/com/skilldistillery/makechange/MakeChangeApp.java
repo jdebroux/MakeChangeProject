8a64903c190d4058e8ed61fd74f51aa95daccfd7
jdebroux <jtdebroux@gmail.com>
2019-07-12 18:03:37 -0600

    User Stories 1,2,3 are working well

--- src/com/skilldistillery/makechange/MakeChangeApp.java
+++ src/com/skilldistillery/makechange/MakeChangeApp.java
@@ -6,7 +6,42 @@ public class MakeChangeApp {
 
 	public static void main(String[] args) {
 		Scanner kb = new Scanner(System.in);
-		System.out.print("How much is the item being purchased? ");
-	}
+		double cost = askUser1(kb);
+		double payment = askUser2(kb);
+		boolean hasEnough$ = tooLittleOrExactMoney(cost, payment);
+		if (hasEnough$ == false) {
+			System.exit(0);
+		}//end if statement
+		else {
+			
+		}// end of else
+		kb.close();
+	}// end of main
 
-}
+	private static boolean tooLittleOrExactMoney(double cost, double payment) {
+		if (payment < cost) {
+			System.out.println("You cannot buy you this item, you are too poor.");
+			return (false);
+		}//end if statement
+		else if (payment == cost) {
+			System.out.println("You paid with exact change.  Have a terrific day!");
+			return (false);
+		}//end else if
+		else {
+			return (true);
+		}//end else
+	}// end tooLittleMoney
+
+	private static double askUser1(Scanner user1) {
+		System.out.print("How much is the item being purchased? ");
+		double cost = user1.nextDouble();
+		return cost;
+	}// end of askUser1
+
+	public static double askUser2(Scanner user2) {
+		System.out.print("How much are you paying with? ");
+		double payment = user2.nextDouble();
+		return payment;
+
+	}// end of askUser2
+}// end of class
